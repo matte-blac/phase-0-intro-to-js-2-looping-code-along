7a90069c466d5fdaf8156f0ae7e35fd906f2df7c
Matthew Bomett <bomettmat2@gmail.com>
2023-09-22 00:36:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const thankYouMessages = []
+
+    for (let i = 0; i < cards.length; i++) {
+        thankYouMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+
+    return thankYouMessages
+}
+
+const thankYouMessages = writeCards(cards)
+console.log(thankYouMessages)
+
+
+
+function countDown(){
+    let i = 10;
+    while (i >= 0 ){
+        console.log(i)
+        i--
+    }
+    return i;
+}
+
+countDown
+
